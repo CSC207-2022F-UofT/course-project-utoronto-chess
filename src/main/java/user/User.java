23cbf397863cbdaab73c4b98be25e3ee32757afb
aidenh20 <aidenhiller4@gmail.com>
2022-11-10 16:24:33 -0500

    added drivers for jdbc

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -3,11 +3,20 @@ package user;
 public class User {
 
     public boolean checkValidUsername(String username) {
-        return true;
+        /*
+         3 <= username.length() <= 15
+         username must only contain alphanumeric characters, underscores, hyphens, and periods.
+         */
+        return username.matches("^[A-Za-z0-9_.-]{3,15}$");
     }
 
     public boolean checkValidPassword(String password) {
-        return true;
+        /*
+         8 <= password.length() <= 20
+         password must contain at least one uppercase letter, one lowercase letter, and one digit.
+         password must have one of the following special characters: !@#$%^&*()_+
+         */
+        return password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$");
     }
 
     public boolean signOut() {
