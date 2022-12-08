c4f8f167eec6811d7ba5c32c4911aea1a8160f77
Jordan Janakievski <jajanak10@gmail.com>
2022-12-08 15:01:40 -0500

    BoardTests update due to fail

--- src/test/java/useCases/board/BoardTests.java
+++ src/test/java/useCases/board/BoardTests.java
@@ -93,8 +93,8 @@ class BoardTests {
     @Test
     @DisplayName("Valid Move")
     void testMovePiece() {
-        int[] start = {1, 0};
-        int[] end = {2, 0};
+        int[] start = {6, 0};
+        int[] end = {5, 0};
         Assertions.assertTrue(this.board.movePiece(start, end));
     }
 
